772627313eb25207f670381bf2f0358d7bec3b97
yasuhiro hotozuka <yasuhiro.hotozuka@nijibox.co.jp>
2022-10-20 15:14:53 +0900

    index追加

--- app/Http/Controllers/TrRecordController.php
+++ app/Http/Controllers/TrRecordController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\TrRecordRequest;
+use App\Models\TrRecord;
+use App\Models\User;
 
 class TrRecordController extends Controller
 {
-    //
+    public function index(Request $request) {
+        $trRecords = TrRecord::all();
+        return view('trrecords.index', compact('trRecords'));
+    }
 }
